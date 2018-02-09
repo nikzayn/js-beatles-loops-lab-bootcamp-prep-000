5d2d1bc1815766d9273ef2de1f6577178e5e53f3
NIKHIL VAIDYAR <nikhilvaidyar1997@gmail.com>
2018-02-09 18:29:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ function theBeatlesPlay(musicians, instruments){
   return array;
 }
 function johnLennonFacts(){
- var facts = []
+ var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+var newfacts;
+
+while(i < )
   
 }
