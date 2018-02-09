e6429e61c416657e56e539a9cd7021fa44ce407b
NIKHIL VAIDYAR <nikhilvaidyar1997@gmail.com>
2018-02-09 18:34:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function johnLennonFacts(){
 var newFacts = [];
 var i = 0;
 while(i < facts.length){
-  newFacts.push(facts[i] + "!!!");
+  newFacts.push(facts[i] +"!!!");
   i++;
 }
 return newFacts;
